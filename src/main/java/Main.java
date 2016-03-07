89b932c3454d74f60b2aa1dd77c6c765830021b1
gangeles5912 <gerry.angeles@experian.com>
2016-03-07 10:31:54 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello Experian");
+    get("/hello", (req, res) -> "Hello Experian Gerry");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+            attributes.put("message", "Hello World Gerry!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
